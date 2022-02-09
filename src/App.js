b2740476dd13bbdb115c539ef9e806300f43c4cf
David Verghese <dj.verghese@gmail.com>
2022-02-09 18:19:56 -0500

    made the paintbrush change color to match current paint color, and dissapear when mouse moves away from canvas

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Canvas from './Components/Canvas/Canvas';
 import Tools from './Components/Tools/Tools';
 import Header from './Components/Header/Header';
-import Cursor from './Components/Cursor/Cursor';
 function App() {
 
 
@@ -10,7 +9,6 @@ function App() {
   
   return (<div className="app">
     
-    <Cursor/>
     <Header />
  
       <Canvas/>
