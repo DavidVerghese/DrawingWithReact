dc2194bb7c34519d7e24ea7b809171d539a123cd
David Verghese <dj.verghese@gmail.com>
2022-02-09 18:34:55 -0500

    changed color scheme

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import './Header.css';
 
 function Header() {
-  return <header><h1>Canvas</h1></header>
+  return <header><h2>Canvas</h2><h2>How to Use This</h2></header>
 }
 export default Header;
