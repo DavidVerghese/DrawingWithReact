eaa94798260bc0d6b33c49e0292d95b77b9e2c95
David Verghese <dj.verghese@gmail.com>
2022-04-07 15:33:10 -0400

    got rid of color modal

--- src/Components/Tools/Tools.jsx
+++ src/Components/Tools/Tools.jsx
@@ -1,6 +1,5 @@
 import './Tools.css';
 import DownloadModal from '../DownloadModal/DownloadModal';
-import ColorModal from '../ColorModal/ColorModal';
 import CustomColor from '../CustomColor/CustomColor';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPaintBrush, faPaintRoller, faBrush  } from "@fortawesome/free-solid-svg-icons"
@@ -17,12 +16,7 @@ function Tools() {
   <FontAwesomeIcon id="medium-paintbrush" className="fa fa-paint-brush" icon={faPaintBrush} />
     <FontAwesomeIcon id="large-paintbrush" className="fa fa-paint-brush" icon={faBrush} />
     <FontAwesomeIcon id="paintroller" className="fa fa-paint-brush" icon={faPaintRoller} />
-    {/* <div className="pick-color-outside">
-      <div className="pick-color"></div>
-    </div> */}
- 
-    {/* <ColorModal /> 
-    <CustomColor/> */}
+
    
 </div>
 }
