13f743cbc76b773f7d4c8fa2e00751f01197ae48
David Verghese <dj.verghese@gmail.com>
2022-04-07 16:17:11 -0400

    worked on line spacing and indentation for header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,15 +6,26 @@ import { faPaintBrush,faUndo, faTrash, faSave } from "@fortawesome/free-solid-sv
 
 function Header(props) {
   const { studio } = props;
-  return <header>
-   
-    {studio ? <>
-      <div className="logo"><Link to="/"><h2>{`< Draw The Line`} <FontAwesomeIcon icon={faPaintBrush} /></h2></Link></div>
-      <FontAwesomeIcon id="undo" icon={faUndo} />
-      <FontAwesomeIcon id="clear" icon={faTrash} />
-      <FontAwesomeIcon id="download" icon={faSave} />
-      
-    </> :  <div className="logo"><h2> Draw The Line  <FontAwesomeIcon icon={faPaintBrush} /></h2></div>}
-  </header>
+  return (
+    <header>
+      {studio ?
+        <>
+          <div className="logo">
+            <Link to="/">
+              <h2>
+                {`< Draw The Line`} <FontAwesomeIcon icon={faPaintBrush} />
+              </h2>
+            </Link>
+          </div>
+          <FontAwesomeIcon id="undo" icon={faUndo} />
+          <FontAwesomeIcon id="clear" icon={faTrash} />
+          <FontAwesomeIcon id="download" icon={faSave} />
+        </> :
+        <div className="logo">
+          <h2>
+            Draw The Line  <FontAwesomeIcon icon={faPaintBrush} />
+          </h2>
+        </div>}
+  </header>)
 }
 export default Header;
