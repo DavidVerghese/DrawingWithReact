09da976e8fca0d9b1089cf35c2fb16d6eb11d7f7
David Verghese <dj.verghese@gmail.com>
2022-04-07 16:08:19 -0400

    fixed formatting for colorpanel.jsx

--- src/Components/ColorPanel/ColorPanel.jsx
+++ src/Components/ColorPanel/ColorPanel.jsx
@@ -1,15 +1,20 @@
 import './ColorPanel.css';
 
 function ColorPanel() {
-  return <div className="color-panel">
-    <div className="colors">
-       <div  id="red-color-field" className="color-field" ></div>
-<div   id="blue-color-field" className="color-field" ></div>
-<div  id="green-color-field" className="color-field" ></div>
-<div   id="yellow-color-field" className="color-field"></div>
-  <p>Custom color: </p>
- <input id="color-picker"  type="color" className="color-picker"/>
+  return (
+    <div className="color-panel">
+      <div className="colors">
+        <div id="red-color-field" className="color-field">
+        </div>
+        <div id="blue-color-field" className="color-field" >
+        </div>
+        <div id="green-color-field" className="color-field" >
+        </div>
+        <div id="yellow-color-field" className="color-field">
+        </div>
+        <p>Custom color: </p>
+        <input id="color-picker" type="color" className="color-picker" />
       </div>
-  </div>
+  </div>)
 }
 export default ColorPanel;
