f83385ea0352c1d30609865303662692f993d9a2
David Verghese <dj.verghese@gmail.com>
2022-04-07 16:20:02 -0400

    worked on line spacing and indetation for homepage.jsx

--- src/Screens/Homepage/Homepage.jsx
+++ src/Screens/Homepage/Homepage.jsx
@@ -4,12 +4,15 @@ import { Link } from "react-router-dom";
 import './Homepage.css';
 
 function Homepage() {
-  return <div className="homepage">
-    <Header />
-    <h2>Ready to paint?</h2>
-    <Link to="/canvas"><button>Enter the studio</button></Link>
-    <img alt="painting" src="https://media1.giphy.com/media/kfip29oFQf9mjhJVa0/200.gif" />
-    <Footer/>
-  </div>
+  return (
+    <div className="homepage">
+      <Header />
+      <h2>Ready to paint?</h2>
+      <Link to="/canvas">
+        <button>Enter the studio</button>
+      </Link>
+      <img alt="painting" src="https://media1.giphy.com/media/kfip29oFQf9mjhJVa0/200.gif" />
+      <Footer/>
+    </div>)
 }
 export default Homepage;
